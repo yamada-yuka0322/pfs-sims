7f7370f33e3e3753334657b05aa41e2ef1504982
ChangHoon Hahn <chhahn@arizona.edu>
2024-12-24 10:38:04 -0500

    revisions to halo.py
    Here's a revised version of the halos.py. The main modifications are: 
    - We want the catalog attributes (redshift, Lbox, particle_mass) separated from the catalog itself (position, velocity etc). I've also set the halo catalog to be an `astropy.table.Table` object, which is better organized than a dictionary. 
    - I've also moved `load_Abacus` as a separate function that reads in the Abacus halos as a `Halos` object. 
    
    There are still details that need to be filled in so go through them when you have the chance.

--- src/pfs-sims/halos.py
+++ src/pfs-sims/halos.py
@@ -7,52 +7,70 @@ to-do:
     - [ ] implement i/o for existing halo catalogs (abacus, uchuu)
     - [ ] documentation for where to find halo catalogs. 
 '''
+import os 
+import numpy as np 
+from astropy.table import Table
 
-class halo():
-    def __init__(self, path):
-        """
-        Initialize the class and load the halo catalog.
 
-        Parameters:
-        - path: path to the directory holding the halo_info files.
+class Halos(object):
+    def __init__(self):
+        """ Flexible class object for general halo catalogs. 
+        
         """
+        # attributes of the halo catalog
         self.redshift = None
-        self.Lbox = None
-        self.particle_mass = None
+        self.Lbox = None              # in units of Mpc/h
+        self.particle_mass = None     # in Msun/h
+        # add additional attributes here as we go.
+        
+        # catalog with halo properties such as position, velocity, mass, virial radius, etc
+        # in an astropy.table.Table 
+        self.catalog = Table()
 
-        self.position = None
-        self.velocity = None
-        self.rvir = None
-        self.rs = None
+    def _check_catalog(self): 
+        ''' check that the minimum set of halo catalog attributes and halo properties are included.
+        '''
+        # check attributes
+        if self.redshift is None: raise ValueError("specify redshift of halo catalog")
+        if self.Lbox is None: raise ValueError("specify box size (Lbox) in Mpc/h")
+        if self.particle_mass is None: raise ValueError("specify particle mass in Msun/h")
 
-        self.sigmav = None
-        self.mass = None
-        self.ids = None
+        # check catalog
+        min_attrs = ['position', 'velocity', 'mass']
+        for attr in min_attrs: 
+            if attr not in self.catalog.columns(): raise ValueError("%s not specified in catalog" % attr)
+        return None
 
-        self.pid = None
-        self.upid = None
 
-        self.path = path
-        self.load_Abacus()
+def load_Abacus(path):
+    """Load the Abacus halo catalog given path of the asdf data files
 
-    def load_Abacus(self):
-        """Loads the halo catalog and sets halo properties."""
-        try:
-            af = asdf.open(self.path + '/halo_info_000.asdf')
-            cat = CompaSOHaloCatalog(self.path, 
-                                     fields = ['v_L2com', 'x_L2com', 'N', 'id', 'r98_L2com', 'r25_L2com', 'sigmav3d_L2com'],
-                                     cleaned=True,
-                                     halo_lc=False)
-            
-            self.velocity  = cat.halos['v_L2com'].data
-            self.position = cat.halos['x_L2com'].data
-            self.mass = cat.halos['N'].data*af['header']['ParticleMassHMsun']
-            self.ids = cat.halos['id'].data
-            self.rvir = cat.halos['r98_L2com'].data
-            self.rs = cat.halos['r25_L2com'].data
-            self.sigmav = cat.halos['sigmav3d_L2com'].data
+    [add links to abacus documentation here]
 
-            self.upid = np.ones(len(self.ids))*(-1)
-            self.pid = self.upid
-        except Exception as e:
-            print(f"Error loading halo catalog: {e}")
+    
+    """
+    import asdf 
+    try:
+        af = asdf.open(path + '/halo_info_000.asdf')
+        cat = CompaSOHaloCatalog(path, 
+                                 fields = ['v_L2com', 'x_L2com', 'N', 'id', 'r98_L2com', 'r25_L2com', 'sigmav3d_L2com'],
+                                 cleaned=True,
+                                 halo_lc=False)
+    except Exception as e:
+        raise ValueError(f"Error loading halo catalog: {e}")
+
+    halos = Halos()
+    # set catalog attributes here
+    halos.redshift = 
+    halos.Lbox = 
+    halos.particle_mass = af['header']['ParticleMassHMsun'] # CHECK UNITS OF THIS
+
+    # set halo properties here
+    halos.catalog['position'] = cat.halos['x_L2com'].data
+    halos.catalog['velocity'] = cat.halos['v_L2com'].data
+    halos.catalog['mass']     = cat.halos['N'].data * af['header']['ParticleMassHMsun']
+    
+    # check that the minimum catalog details are set
+    halos._check_catalog()
+    
+    return halos
